0e57e78682fa403680257853160a1598ad78ff58
Geist <1319119251@qq.com>
2024-02-29 10:22:30 +0800

    daily

--- 01_create_table_n_records/first_sqlalchemy.py
+++ 01_create_table_n_records/first_sqlalchemy.py
@@ -1,13 +1,14 @@
 import sqlalchemy
 
-engine = sqlalchemy.create_engine("mysql+pymysql://root:abcd=1234@localhost:3306/itcast")
+engine = sqlalchemy.create_engine("mysql+pymysql://root:Zc.1319119251@localhost:3306/test")
+
 
 conn = engine.connect()
-
-query = sqlalchemy.text("select * from employee")
-
+#
+query = sqlalchemy.text("select * from test")
+#
 result = conn.execute(query)
-
+#
 for row_set in result:
     print(row_set)
 
