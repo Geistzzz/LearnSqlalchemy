c73ffedc982cdaa916558a6926b379f4fe794457
Geistzzz <lilizhangchi@gmail.com>
2024-04-05 01:46:34 +0800

    daily

--- 03_第二种连接方式_增_删_改/update.py
+++
@@ -1,9 +0,0 @@
-from db_init import engine, person_table
-
-with engine.connect() as conn:
-    # 不带条件
-    # update_query = person_table.update().values(address='aaa')
-    # 带条件
-    update_query = person_table.update().values(address='bbb').where(person_table.c.id == 6 )
-    conn.execute(update_query)
-    conn.commit()
--- 08_第三种连接方式_新的映射方式/query.py
+++
@@ -1,9 +0,0 @@
-from db_init4 import Session, Customer
-
-session = Session()
-
-c = Customer(name='AAA', birthday='2000-01-05')  # 实例化一个对象
-
-session.add(c)  # 添加到这个表里面
-
-session.commit()
